f808dcd6badbaa0b84d209deabd073da108f33fa
SJ <hsj9360@gmail.com>
2021-07-16 01:01:04 +0900

    video list w/ title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+  // 컴포넌트가 업데이트 될때마다 호출됨 / []마운트만 되었을때 호출함
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD94CzmXXJryyoGWXqyxF_4nCopt28r94M',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
