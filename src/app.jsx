af30d9c77a4031a89b507a1face9925f9923dd42
SJ <hsj9360@gmail.com>
2021-07-23 01:04:54 +0900

    using axios

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
@@ -12,20 +12,23 @@ function App({ youtube }) {
     setSelectedVideo(video);
   };
 
-  const search = query => {
-    setSelectedVideo(null);
-
-    youtube
-      .search(query) //
-      .then(videos => setVideos(videos));
-  };
+  // useCallback 은 메모리의 영향이 갈수도있다.
+  const search = useCallback(
+    query => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then(videos => setVideos(videos));
+    },
+    [youtube]
+  );
 
   // 컴포넌트가 업데이트 될때마다 호출됨 / []마운트만 되었을때 호출함
   useEffect(() => {
     youtube
       .mostPopular() //
       .then(videos => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
   return (
     <div className={styles.app}>
